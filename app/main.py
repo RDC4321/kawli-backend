82244d3844934af63f43d3d71e6ddeb55067a1a7
Reeni <reenicawaling12@gmail.com>
2026-03-13 12:37:16 +0400

    enable cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api import utils
 from app.api import search
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Kawli API",
@@ -10,3 +11,11 @@ app = FastAPI(
 
 app.include_router(utils.router)
 app.include_router(search.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all for now
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
