b4695295215ec436338da36e6b133dcc7756ce64
Reeni <reenicawaling12@gmail.com>
2026-03-13 12:38:10 +0400

    fix cors middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import utils
 from app.api import search
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(
     title="Kawli API",
@@ -9,13 +9,15 @@ app = FastAPI(
     version="1.0.0"
 )
 
-app.include_router(utils.router)
-app.include_router(search.router)
-
+# CORS middleware (must be early)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow all for now
+    allow_origins=["*"],  # allow Framer frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Routers
+app.include_router(utils.router)
+app.include_router(search.router)
